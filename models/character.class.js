733d4f34abba5880f7829e04405b25f2c5769c0e
SABRINA FRITZ <SABRINA_FRITZ@GMX.NET>
2024-12-14 20:05:50 +0100

    character swims up and down

--- models/character.class.js
+++ models/character.class.js
@@ -8,6 +8,40 @@ class Character extends MovableObject {
         'img/1.Sharkie/3.Swim/5.png',
         'img/1.Sharkie/3.Swim/6.png',
     ];
+
+    IMAGES_STANDING = [
+        'img/1.Sharkie/1.IDLE/1.png',
+        'img/1.Sharkie/1.IDLE/2.png',
+        'img/1.Sharkie/1.IDLE/3.png',
+        'img/1.Sharkie/1.IDLE/4.png',
+        'img/1.Sharkie/1.IDLE/5.png',
+        'img/1.Sharkie/1.IDLE/6.png',
+        'img/1.Sharkie/1.IDLE/7.png',
+        'img/1.Sharkie/1.IDLE/8.png',
+        'img/1.Sharkie/1.IDLE/9.png',
+        'img/1.Sharkie/1.IDLE/10.png',
+        'img/1.Sharkie/1.IDLE/11.png',
+        'img/1.Sharkie/1.IDLE/12.png',
+        'img/1.Sharkie/1.IDLE/13.png',
+    ]
+
+    IMAGES_SLEEPING = [
+        'img/1.Sharkie/2.Long_IDLE/i1.png',
+        'img/1.Sharkie/2.Long_IDLE/I2.png',
+        'img/1.Sharkie/2.Long_IDLE/I3.png',
+        'img/1.Sharkie/2.Long_IDLE/I4.png',
+        'img/1.Sharkie/2.Long_IDLE/I5.png',
+        'img/1.Sharkie/2.Long_IDLE/I6.png',
+        'img/1.Sharkie/2.Long_IDLE/I7.png',
+        'img/1.Sharkie/2.Long_IDLE/I8.png',
+        'img/1.Sharkie/2.Long_IDLE/I9.png',
+        'img/1.Sharkie/2.Long_IDLE/I10.png',
+        'img/1.Sharkie/2.Long_IDLE/I11.png',
+        'img/1.Sharkie/2.Long_IDLE/I12.png',
+        'img/1.Sharkie/2.Long_IDLE/I13.png',
+        'img/1.Sharkie/2.Long_IDLE/I14.png'
+    ]
+
     world; 
 
     swimming_sound = new Audio('audio/big-bubble-1-169075.mp3');
@@ -15,7 +49,9 @@ class Character extends MovableObject {
     constructor() {
         super().loadImage('../img/1.Sharkie/1.IDLE/1.png');
         this.loadImages(this.IMAGES_SWIMMING);
-
+        this.loadImages(this.IMAGES_STANDING);
+        this.loadImages(this.IMAGES_SLEEPING);
+        this.applyGravity();
         this.animate();
 
         this.height = 300;
@@ -27,34 +63,51 @@ class Character extends MovableObject {
     animate() {
         setInterval(() => {
             if (this.world.keyboard.RIGHT && this.x < this.world.level.level_end_x) {
-                this.x += this.speed;
-                this.otherDirection = false;
+                this.moveRight();
                 this.swimming_sound.play();
-
             }
             if (this.world.keyboard.LEFT && this.x > 0 ) {
                 this.x -= this.speed;
                 this.otherDirection = true;
                 this.swimming_sound.play();
             }
+
             this.world.camera_x = -this.x + 100;
         }, 1000/60);
 
         setInterval(() => {
             if (this.world.keyboard.RIGHT || this.world.keyboard.LEFT) {
-              
-                //swim moves
                 this.playAnimation(this.IMAGES_SWIMMING);
-                // let i = this.currentImage % this.IMAGES_SWIMMING.length;
-                // let path = this.IMAGES_SWIMMING[i];
-                // this.img = this.imageCache[path];
-                // this.currentImage++
             }
-      
         }, 200)
-    }
 
-    jump() {
 
+        setInterval(() => {
+            if (!this.world.keyboard.RIGHT && !this.world.keyboard.LEFT && !this.world.keyboard.UP && !this.world.keyboard.DOWN && !this.world.keyboard.SPACE) {
+                this.playAnimation(this.IMAGES_STANDING);
+            }
+        }, 200)
+
+        setInterval(() => {
+            if (this.y > 210 && !this.world.keyboard.RIGHT && !this.world.keyboard.LEFT && !this.world.keyboard.UP && !this.world.keyboard.DOWN && !this.world.keyboard.SPACE) {
+                this.playAnimation(this.IMAGES_SLEEPING);
+                console.log('keine Taste ' + this.y);
+            }
+        }, 2000)
+
+        setInterval(() => {
+            if 
+            ( this.y <= 210 && this.world.keyboard.UP && this.y >= -80) {
+                console.log('Pfeil hoch, über Boden');
+                this.playAnimation(this.IMAGES_STANDING);
+                this.speedY = 5;
+            }
+            else if(this.y > 210 && this.world.keyboard.UP) {
+                console.log('Pfeil hoch, am Boden');
+                this.playAnimation(this.IMAGES_STANDING);
+                this.y += this.speedY;
+                this.speedY += 15;
+            } 
+        },200)
     }
 }
--- models/movable-object.class.js
+++ models/movable-object.class.js
@@ -8,6 +8,30 @@ class MovableObject {
     currentImage = 0;
     speed = 0.25;
     otherDirection = false;
+    speedY = 0;
+    acceleration = 0.2;
+
+    applyGravity() {
+        setInterval(() => {
+            if (this.isAboveGround()) {
+                this.y -= this.speedY;
+                this.speedY -= this.acceleration;
+            }
+        }, 1000/25)
+    }
+
+    isAboveGround() {
+        return this.y < 210;
+    }
+
+    upWhileIsOnGround() {
+        setInterval(() => {
+            if (!this.isAboveGround()) {
+                this.y += this.speedY;
+                this.speedY += this.acceleration;
+            }
+        }, 1000/25)
+    }
 
     loadImage(path) {
         this.img = new Image();
@@ -23,14 +47,20 @@ class MovableObject {
     }
 
     playAnimation(images) {
-        let i = this.currentImage % this.IMAGES_SWIMMING.length;
+        let i = this.currentImage % images.length;
         let path = images[i];
         this.img = this.imageCache[path];
         this.currentImage++
     }
 
+    stopAnimation(images) {
+        
+    }
+
     moveRight() {
-       console.log('Moving right');
+        this.x += this.speed;
+        this.otherDirection = false;
+        
         
     }
 
